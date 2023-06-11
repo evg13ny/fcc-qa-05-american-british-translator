3088eafdfd021560e86fee4d2246350d199626c6
Evgeny Polyakov <evg_pol@yahoo.com>
2023-06-11 13:33:46 +0300

    first commit

--- components/translator.js
+++ components/translator.js
@@ -3,8 +3,115 @@ const americanToBritishSpelling = require('./american-to-british-spelling.js');
 const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
 
+const reverseDict = obj => {
+    return Object.assign(
+        {},
+        ...Object.entries(obj).map(([k, v]) => ({ [v]: k }))
+    );
+}
+
 class Translator {
 
+    toBritishEnglish(text) {
+        const dict = { ...americanOnly, ...americanToBritishSpelling };
+        const titles = americanToBritishTitles;
+        const timeRegex = /([1-9]|1[012]):[0-5][0-9]/g;
+        const translation = this.translate(
+            text,
+            dict,
+            titles,
+            timeRegex,
+            'toBritish'
+        );
+
+        if (!translation) {
+            return text;
+        }
+
+        return translation;
+    }
+
+    toAmericanEnglish(text) {
+        const dict = { ...britishOnly, ...reverseDict(americanToBritishSpelling) };
+        const titles = reverseDict(americanToBritishTitles);
+        const timeRegex = /([1-9]|1[012]).[0-5][0-9]/g;
+        const translation = this.translate(
+            text,
+            dict,
+            titles,
+            timeRegex,
+            'toAmerican'
+        );
+
+        if (!translation) {
+            return text;
+        }
+
+        return translation;
+    }
+
+    translate(text, dict, titles, timeRegex, locale) {
+        const lowerText = text.toLowerCase();
+        const matchesMap = {};
+
+        Object.entries(titles).map(([k, v]) => {
+            if (lowerText.includes(k)) {
+                matchesMap[k] = v.charAt(0).toUpperCase() + v.slice(1);
+            }
+        });
+
+        const wordsWithSpaces = Object.fromEntries(
+            Object.entries(dict).filter(([k, v]) => k.includes(' '))
+        );
+
+        Object.entries(wordsWithSpaces).map(([k, v]) => {
+            if (lowerText.includes(k)) {
+                matchesMap[k] = v;
+            }
+        });
+
+        lowerText.match(/(\w+([-'])(\w+)?['-]?(\w+))|]\w+/g).forEach(word => {
+            if (dict[word]) matchesMap[word] = dict[word]
+        });
+
+        const matchedTime = lowerText.match(timeRegex);
+
+        if (matchedTime) {
+            matchedTime.map((e) => {
+                if (locale === 'toBritish') {
+                    return (matchedTime[e] = e.replace(':', '.'));
+                } else {
+                    return (matchedTime[e] = e.replace('.', ':'));
+                }
+            });
+        }
+
+        if (Object.keys(matchesMap).length === 0) return null;
+
+        const translation = this.toBritishEnglish.replaceAll(text, matchesMap);
+
+        const translationWithHighlight = this.replaceAllWithHighlight(
+            text,
+            matchesMap
+        );
+
+        return [translation, translationWithHighlight];
+    }
+
+    replaceAll(text, matchesMap) {
+        const regex = new RegExp(Object.keys(matchesMap).join('|'), 'gi');
+
+        return text.replace(regex, matched => matchesMap[matched.toLowerCase()]);
+    }
+
+    replaceAllWithHighlight(text, matchesMap) {
+        const regex = new RegExp(Object.keys(matchesMap).join('|'), 'gi');
+
+        return text.replace(regex, matched => {
+            return `<span class="highlight">${matchesMap[matched.toLowerCase()]
+                }</span>`;
+        });
+    }
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,33 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      const { text, locale } = req.body;
+
+      if (!locale || text === undefined) {
+        res.json({ error: 'Required field(s) missing' });
+        return;
+      }
+
+      if (text === '') {
+        res.json({ error: 'No text to translate' });
+        return;
+      }
+
+      let result = '';
+
+      if (locale === 'american-to-british') {
+        result = translator.toBritishEnglish(text);
+      } else if (locale === 'british-to-american') {
+        result = translator.toAmericanEnglish(text);
+      } else {
+        res.json({ error: 'Invalid value for locale field' });
+        return;
+      }
+
+      if (result === text || !result) {
+        res.json({ text, result: 'Everything looks good to me!' });
+      } else {
+        res.json({ text, result: result[1] });
+      }
     });
 };
